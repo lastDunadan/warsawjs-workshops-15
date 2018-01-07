5382fd9eac0097dc8ab73f594b537a10e8866e16
Pawel Modzelewski <pawel.modzelewski@creativemonsters.pl>
2018-01-07 14:08:25 +0100

    step 7

--- script.js
+++ script.js
@@ -25,25 +25,47 @@ document.addEventListener('DOMContentLoaded', function () {
       col3 = fields[2].className + fields[5].className + fields[8].className,
 
       diag1 = fields[0].className + fields[4].className + fields[8].className,
-      diag2 = fields[6].className + fields[4].className + fields[2].className;
+      diag2 = fields[6].className + fields[4].className + fields[2].className,
 
-    if (row1 === 'redredred' || row2 === 'redredred' || row3 === 'redredred' ||
-        col1 === 'redredred' || col2 === 'redredred' || col3 === 'redredred' ||
-        diag1 === 'redredred' || diag2 === 'redredred') {
-      alert('Red Wins!');
-      return;
+      boardCheck = [
+        row1, row2, row3,
+        col1, col2, col3,
+        diag1, diag2
+      ];
+
+    if (boardCheck.includes('redredred')) {
+      setTimeout(function () {
+        alert('Red Wins');
+        return;
+      }, 100);
     }
 
-    if (row1 === 'blueblueblue' || row2 === 'blueblueblue' || row3 === 'blueblueblue' ||
-        col1 === 'blueblueblue' || col2 === 'blueblueblue' || col3 === 'blueblueblue' ||
-        diag1 === 'blueblueblue' || diag2 === 'blueblueblue') {
-      alert('Blue Wins!');
-      return;
+    if (boardCheck.includes('blueblueblue')) {
+      setTimeout(function () {
+        alert('Blue Wins');
+        return;
+      }, 100);
     }
+
+//    if (row1 === 'redredred' || row2 === 'redredred' || row3 === 'redredred' ||
+//        col1 === 'redredred' || col2 === 'redredred' || col3 === 'redredred' ||
+//        diag1 === 'redredred' || diag2 === 'redredred') {
+//      alert('Red Wins!');
+//      return;
+//    }
+//
+//    if (row1 === 'blueblueblue' || row2 === 'blueblueblue' || row3 === 'blueblueblue' ||
+//        col1 === 'blueblueblue' || col2 === 'blueblueblue' || col3 === 'blueblueblue' ||
+//        diag1 === 'blueblueblue' || diag2 === 'blueblueblue') {
+//      alert('Blue Wins!');
+//      return;
+//    }
 
     if (emptyFields === 0) {
-      alert('Tie');
-      return;
+      setTimeout(function () {
+        alert('Tie');
+        return;
+      }, 100);
     }
   }
 
