e17c1a9aa67760f4b84c8e2bddfc117b2f5dbcc2
Pawel Modzelewski <pawel.modzelewski@creativemonsters.pl>
2018-01-07 11:28:56 +0100

    step 1

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 /*global console*/
-/*global $, jQuery, alert*/
 
-(function () {
+document.addEventListener('DOMContentLoaded', function () {
   'use strict';
 
-  // Dodaj treść skryptu!
+  function initGame() {
+    var fields = document.querySelectorAll('.board > div');
+    fields.forEach(field => field.addEventListener('click', fieldClickHandler));
+  }
 
-}());
+  function fieldClickHandler() {
+    classList.add('background-color:red', this);
+    console.log('Hello', this);
+  }
+
+  initGame();
+
+});
