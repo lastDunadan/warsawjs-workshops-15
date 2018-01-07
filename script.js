54cc23d863f9a9ea9c827c9e1493d750c8758cfd
Pawel Modzelewski <pawel.modzelewski@creativemonsters.pl>
2018-01-07 12:51:40 +0100

    step 6

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /*global console*/
 /*global alert*/
+/*global document*/
 
 document.addEventListener('DOMContentLoaded', function () {
   'use strict';
@@ -10,6 +11,41 @@ document.addEventListener('DOMContentLoaded', function () {
   },
     currentPlayer,
     emptyFields;
+
+  function checkWinner() {
+
+    var fields = document.querySelectorAll('.board > div'),
+
+      row1 = fields[0].className + fields[1].className + fields[2].className,
+      row2 = fields[3].className + fields[4].className + fields[5].className,
+      row3 = fields[6].className + fields[7].className + fields[8].className,
+
+      col1 = fields[0].className + fields[3].className + fields[6].className,
+      col2 = fields[1].className + fields[4].className + fields[7].className,
+      col3 = fields[2].className + fields[5].className + fields[8].className,
+
+      diag1 = fields[0].className + fields[4].className + fields[8].className,
+      diag2 = fields[6].className + fields[4].className + fields[2].className;
+
+    if (row1 === 'redredred' || row2 === 'redredred' || row3 === 'redredred' ||
+        col1 === 'redredred' || col2 === 'redredred' || col3 === 'redredred' ||
+        diag1 === 'redredred' || diag2 === 'redredred') {
+      alert('Red Wins!');
+      return;
+    }
+
+    if (row1 === 'blueblueblue' || row2 === 'blueblueblue' || row3 === 'blueblueblue' ||
+        col1 === 'blueblueblue' || col2 === 'blueblueblue' || col3 === 'blueblueblue' ||
+        diag1 === 'blueblueblue' || diag2 === 'blueblueblue') {
+      alert('Blue Wins!');
+      return;
+    }
+
+    if (emptyFields === 0) {
+      alert('Tie');
+      return;
+    }
+  }
 
   function fieldClickHandler() {
     var playerClass = playerClasses[currentPlayer];
@@ -29,10 +65,7 @@ document.addEventListener('DOMContentLoaded', function () {
     this.removeEventListener('click', fieldClickHandler);
     console.log('Fields left: ' + emptyFields);
 
-    if (emptyFields === 0) {
-      alert('Endo fo the Game');
-    }
-
+    checkWinner();
   }
 
   function initGame() {
