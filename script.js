c80d815e0e103a0d01a41ece6f7ab1a20617523a
Pawel Modzelewski <pawel.modzelewski@creativemonsters.pl>
2018-01-07 12:04:28 +0100

    step 5

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
 /*global console*/
+/*global alert*/
 
 document.addEventListener('DOMContentLoaded', function () {
   'use strict';
 
-  function initGame() {
-    var fields = document.querySelectorAll('.board > div');
-    fields.forEach(field => field.addEventListener('click', fieldClickHandler));
-  }
+  var playerClasses = {
+    'playerA' : 'red',
+    'playerB' : 'blue'
+  },
+    currentPlayer,
+    emptyFields;
 
   function fieldClickHandler() {
-    this.classList.add('red');
-    console.log('Hello', this);
+    var playerClass = playerClasses[currentPlayer];
+    this.classList.add(playerClass);
+
+    //Skrocony zapis IF
+    //currentPlayer = currentPlayer === 'playerA' ? 'playerB' : 'playerA';
+
+    emptyFields = emptyFields - 1;
+
+    if (currentPlayer === 'playerA') {
+      currentPlayer = 'playerB';
+    } else {
+      currentPlayer = 'playerA';
+    }
+
+    this.removeEventListener('click', fieldClickHandler);
+    console.log('Fields left: ' + emptyFields);
+
+    if (emptyFields === 0) {
+      alert('Endo fo the Game');
+    }
+
+  }
+
+  function initGame() {
+    var fields = document.querySelectorAll('.board > div');
+
+    currentPlayer = 'playerA';
+    emptyFields = 9;
+    fields.forEach(function (field) {
+      field.addEventListener('click', fieldClickHandler);
+    });
+
+    //fields.forEach(field (=>) field.addEventListener('click', fieldClickHandler));
   }
 
   initGame();
